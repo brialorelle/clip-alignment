5d23d7a798b28d442e71c18aea1a6e393ec35884
Zi Yin <yinzi@node16-ccncluster.stanford.edu>
2024-04-02 20:12:54 -0700

    change clip to clip-as-service

--- babyview_exps/clip_on_all_frames.py
+++ babyview_exps/clip_on_all_frames.py
@@ -1,75 +1,52 @@
 import os
 import csv
-import clip
-import torch
 import numpy as np
-
 from PIL import Image
 import argparse
+from clip_client import Client
+from docarray import Document, DocumentArray
+
+# Initialize the argparse
 args_parser = argparse.ArgumentParser()
 args_parser.add_argument("--video_file", type=str, required=True)
 args_parser.add_argument("--csv_file", type=str, required=True)
-args_parser.add_argument("--device", type=str, default="cpu")
+args_parser.add_argument("--server_url", type=str, default="grpc://0.0.0.0:51000")
 args_parser.add_argument("--save_root_dir", type=str, required=False)
 
 args = args_parser.parse_args()
-# Main script starts here
+
+# Initialize the CLIP client
+client = Client(args.server_url)
+
 csv_file = args.csv_file
 video_file = args.video_file
-device = args.device
-# name without extension
-save_root_dir = args.save_root_dir
-if save_root_dir is None:
-    save_root_dir = "./"
+save_root_dir = args.save_root_dir or "./"
 
 video_file_name = os.path.splitext(os.path.basename(video_file))[0]
 
 def save_embedding(embedding, dir_path, file_name):
     os.makedirs(dir_path, exist_ok=True)
     path = os.path.join(dir_path, file_name)
-    np.save(path, embedding.detach().cpu().numpy())
+    np.save(path, embedding)
     return path
 
-
-def calculate_dot_product(frame_folder, text, utterance_no, model, token_limit=77):
+def calculate_dot_product(frame_folder, text, utterance_no):
     results = []
-    # Split the text into chunks within the token limit
-    text_chunks = [text[i:i+token_limit] for i in range(0, len(text), token_limit)]
-    combined_text_features = None
+    # Encode the text to get its embedding
+    text_embedding = client.encode([text])[0]
+    text_embedding_path = save_embedding(text_embedding, os.path.join(save_root_dir, f'all_embeddings/text_embeddings/{video_file_name}'), f'{utterance_no}_text_embedding.npy')
     
-    for chunk in text_chunks:
-        # Preprocess and encode each text chunk
-        text_input = clip.tokenize([chunk]).to(device)
-        text_features_chunk = model.encode_text(text_input)
-        text_features_chunk /= text_features_chunk.norm(dim=-1, keepdim=True)
-        
-        # Combine encoded features of all chunks by averaging
-        if combined_text_features is None:
-            combined_text_features = text_features_chunk
-        else:
-            combined_text_features += text_features_chunk
-   
-    # Average the combined text features if text was split into chunks
-    if len(text_chunks) > 1:
-        combined_text_features /= len(text_chunks)
-    text_embedding_path = save_embedding(combined_text_features, os.path.join(save_root_dir, f'all_embeddings/text_embeddings/{video_file_name}'), f'{utterance_no}_text_embedding.npy')
-    # Iterate through each frame in the folder
     for frame in os.listdir(frame_folder):
         if frame.endswith(".jpg"):
             frame_path = os.path.join(frame_folder, frame)
             # Load and preprocess the frame
-            frame_image = preprocess(Image.open(frame_path)).unsqueeze(0).to(device)
+            frame_image = Document(uri=frame_path).convert_uri_to_image_tensor()
+            # Encode the frame to get its embedding
+            image_embedding = client.encode([frame_image])[0]
+            image_embedding_path = save_embedding(image_embedding, os.path.join(save_root_dir, f'all_embeddings/image_embeddings/{video_file_name}/{utterance_no}'), f'{os.path.splitext(frame)[0]}_image_embedding.npy')
             
-            # Encode the frame
-            frame_features = model.encode_image(frame_image)
-            frame_features /= frame_features.norm(dim=-1, keepdim=True)
-            image_embedding_path = save_embedding(frame_features, os.path.join(save_root_dir, f'all_embeddings/image_embeddings/{video_file_name}/{utterance_no}'), f'{os.path.splitext(frame)[0]}_image_embedding.npy')
-
             # Calculate dot product
-            dot_product = (text_embedding_path @ frame_features.T).detach().cpu().numpy()[0][0]
-            del frame_image
-            del frame_features
-            torch.cuda.empty_cache()
+            dot_product = np.dot(text_embedding, image_embedding)
             
             results.append({
                 "utterance_no": utterance_no,
@@ -81,22 +58,19 @@ def calculate_dot_product(frame_folder, text, utterance_no, model, token_limit=7
             })
     return results
 
-
-# Load the CLIP model
-model, preprocess = clip.load("ViT-B/32", device=device)
-model.eval()
 final_results = []
 with open(csv_file, mode='r') as file:
     csv_reader = csv.DictReader(file)
     for row in csv_reader:
-        output_dir =  os.path.join(save_root_dir, f'output_frames/{video_file_name}/{row["utterance_no"]}')
+        output_dir = os.path.join(save_root_dir, f'output_frames/{video_file_name}/{row["utterance_no"]}')
         os.makedirs(output_dir, exist_ok=True)
-        results = calculate_dot_product(output_dir, row['text'], row['utterance_no'], model=model)
+        results = calculate_dot_product(output_dir, row['text'], row['utterance_no'])
         final_results.extend(results)
-        
+
 output_path = "all_result_csv_files"
 output_dir = os.path.join(save_root_dir, output_path, video_file_name)
 os.makedirs(output_dir, exist_ok=True)
+
 # Write final results to a single CSV file
 with open(os.path.join(output_dir,'clip_final_results.csv'), mode='w', newline='') as result_file:
     writer = csv.DictWriter(result_file, fieldnames=["utterance_no", "frame", "text", "dot_product", "text_embedding_path", "image_embedding_path"])
--- babyview_exps/multithread_all_videos_clip_align.py
+++ babyview_exps/multithread_all_videos_clip_align.py
@@ -3,48 +3,54 @@ from glob import glob
 import re
 from tqdm import tqdm
 import argparse
+import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import torch
+
+# Setup argument parser
 args_parser = argparse.ArgumentParser()
 args_parser.add_argument("--babyview_folder", type=str, required=True)
 args_parser.add_argument("--babyview_transcript_folder", type=str, required=True)
 args_parser.add_argument("--output_root_dir", type=str, required=True)
+args_parser.add_argument("--max_workers", type=int, default=4, help="Maximum number of worker threads")
 args = args_parser.parse_args()
 
-
-
 babyview_video_folder = args.babyview_folder
-babyview_transcript_fodler = args.babyview_transcript_folder
+babyview_transcript_folder = args.babyview_transcript_folder
 output_root_dir = args.output_root_dir
+max_workers = args.max_workers
 
 cwd_path = os.getcwd()
-# babyview_video_folder = "/data/babyview/Babyview_Main"
-# babyview_transcript_fodler = "/data/babyview/transcripts/Babyview_Main"
-# output_root_dir = "/data/babyview/all_clip_results"
-
 os.makedirs(output_root_dir, exist_ok=True)
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
 all_subject_number_list = sorted(os.listdir(babyview_video_folder))
-
 total_number_of_videos = len(glob(os.path.join(babyview_video_folder, "*", "*.MP4")))
 print(f"The total number of videos found in the main folder: {total_number_of_videos}")
 
-for subjcet in tqdm(all_subject_number_list):
-    all_mp4_files = glob(os.path.join(babyview_video_folder, subjcet, "*.MP4"))
-    for mp4_full_path in tqdm(all_mp4_files):
-        mp4_filename = os.path.basename(mp4_full_path)
-        transcript_filename = re.sub(r"\.MP4$", ".csv", mp4_filename)
-        transcript_full_path = os.path.join(babyview_transcript_fodler,subjcet, transcript_filename)
-        output_full_path = os.path.join(output_root_dir, subjcet)
-        os.makedirs(output_full_path, exist_ok=True)
-        os.system(f"python3 {cwd_path}/extract_all_frames.py --video_file {mp4_full_path} --csv_file {transcript_full_path} --save_root_dir {output_full_path}")
-        os.system(f"python3 {cwd_path}/clip_on_all_frames.py --video_file {mp4_full_path} --csv_file {transcript_full_path} --device {device} --save_root_dir {output_full_path}")
-        # Skip if transcript file for this video not found
-        try:
-            assert os.path.exists(transcript_full_path)
+def process_video(mp4_full_path, subject):
+    mp4_filename = os.path.basename(mp4_full_path)
+    transcript_filename = re.sub(r"\.MP4$", ".csv", mp4_filename)
+    transcript_full_path = os.path.join(babyview_transcript_folder, subject, transcript_filename)
+    output_full_path = os.path.join(output_root_dir, subject)
+    os.makedirs(output_full_path, exist_ok=True)
 
-        except:
-            print(f"[WARNING]: Transcript file not found for {mp4_filename}")
-            continue
+    if not os.path.exists(transcript_full_path):
+        print(f"[WARNING]: Transcript file not found for {mp4_filename}")
+        return
     
+    # Replace with the correct script path and its arguments
+    command = f"python3 {cwd_path}/clip_on_all_frames.py --video_file {mp4_full_path} --csv_file {transcript_full_path} --save_root_dir {output_full_path}"
+    os.system(command)
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+    # Submit all video processing tasks to the executor
+    futures = []
+    for subject in all_subject_number_list:
+        all_mp4_files = glob(os.path.join(babyview_video_folder, subject, "*.MP4"))
+        for index, mp4_full_path in enumerate(all_mp4_files):
+            futures.append(executor.submit(process_video, mp4_full_path, subject))
+    
+    progress_bar = tqdm(total=len(futures))
+    # Progress bar for futures
+    for future in concurrent.futures.as_completed(futures):
+        progress_bar.update(1)
+        future.result() # This line is optional, it will raise exceptions if any occurred during the function execution
