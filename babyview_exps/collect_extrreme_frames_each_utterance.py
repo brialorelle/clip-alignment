fa5de3528a686db696d53e18ce59cf5358fe658a
ZiYin <myinzi123@gmail.com>
2024-04-10 18:49:37 -0700

    Fix bug where only one video per subject was processed

--- babyview_exps/collect_extrreme_frames_each_utterance.py
+++ babyview_exps/collect_extrreme_frames_each_utterance.py
@@ -16,15 +16,11 @@ all_subject_number_list = [subject for subject in all_subject_number_list if sub
 total_number_of_videos = sum([len(glob(os.path.join(babyview_video_folder, subject, "*.MP4")) ) for subject in all_subject_number_list])
 
 
-progress_bar = tqdm(total=total_number_of_videos)
 all_chosen_frames_df = pd.DataFrame()
-for subject in all_subject_number_list:
-    # create a single folder
-    os.makedirs(os.path.join(output_root_dir, "all_clip_results","chosen_frames"), exist_ok=True)
+for subject in tqdm(all_subject_number_list):
     all_mp4_files = glob(os.path.join(babyview_video_folder, subject, "*.MP4"))
-    subject_df = pd.DataFrame()
-    # merge all single mp4 csv files into one, rename the utterance_no in format: {video_name}_{utterance_no}
-    for mp4_full_path in all_mp4_files:
+    results = []
+    for mp4_full_path in tqdm(all_mp4_files):
         video_file_name = os.path.splitext(os.path.basename(mp4_full_path))[0]
         output_csv_dir = os.path.join(output_root_dir, "all_clip_results",subject, "all_result_csv_files", video_file_name,'clip_final_results.csv')
         single_video_df = pd.read_csv(output_csv_dir)
@@ -32,28 +28,24 @@ for subject in all_subject_number_list:
         single_video_df = single_video_df.dropna(subset=['dot_product'])
         # rename the utterance_no in format: {video_name}_{utterance_no}
         single_video_df['utterance_no'] = single_video_df['utterance_no'].apply(lambda x: f"{video_file_name}_{x}")
-        # merge
-        subject_df = pd.concat([subject_df, single_video_df], ignore_index=True)
-    utterances = single_video_df['utterance_no'].unique()
-    results = []
-    for utterance_no in utterances:
-        utterance_df = single_video_df[single_video_df['utterance_no'] == utterance_no]
-        max_frame = utterance_df.loc[utterance_df['dot_product'].idxmax()]
-        min_frame = utterance_df.loc[utterance_df['dot_product'].idxmin()]
-        max_dot_product = max_frame['dot_product']
-        min_dot_product = min_frame['dot_product']
-        if max_dot_product == min_dot_product:
-            # print(f"Same dot product for {utterance_no}")
-            continue
-        result = {
-            "utterance_no": utterance_no,
-            "max_frame": max_frame['frame'],
-            "max_dot_product": max_dot_product,
-            "min_frame": min_frame['frame'],
-            "min_dot_product": min_dot_product
-        }
-        results.append(result)
-    
+        utterances = single_video_df['utterance_no'].unique()
+        for utterance_no in utterances:
+            utterance_df = single_video_df[single_video_df['utterance_no'] == utterance_no]
+            max_frame = utterance_df.loc[utterance_df['dot_product'].idxmax()]
+            min_frame = utterance_df.loc[utterance_df['dot_product'].idxmin()]
+            max_dot_product = max_frame['dot_product']
+            min_dot_product = min_frame['dot_product']
+            if max_dot_product == min_dot_product:
+                # print(f"Same dot product for {utterance_no}")
+                continue
+            result = {
+                "utterance_no": utterance_no,
+                "max_frame": max_frame['frame'],
+                "max_dot_product": max_dot_product,
+                "min_frame": min_frame['frame'],
+                "min_dot_product": min_dot_product
+            }
+            results.append(result)
     extreme_frame_df = pd.DataFrame(results)
     sample_number = 10
     if len(extreme_frame_df) < sample_number:
@@ -66,6 +58,7 @@ for subject in all_subject_number_list:
     chosen_frame_df = extreme_frame_df.sample(sample_number)
     all_chosen_frames_df = pd.concat([all_chosen_frames_df, chosen_frame_df], ignore_index=True)
 # %% Copy the chosen frames to a new folder
+os.makedirs(os.path.join(output_root_dir, "all_clip_results","chosen_frames"), exist_ok=True)
 save_frame_dir = os.path.join(output_root_dir, "all_clip_results","chosen_frames")
 os.makedirs(save_frame_dir, exist_ok=True)
 # save the chosen highest and lowest dot product frames for each subject
@@ -84,3 +77,4 @@ for index, row in all_chosen_frames_df.iterrows():
     # copy the frame to the save_frame_dir
     copy2(max_frame_source_path, max_frame_dest_path)
     copy2(min_frame_source_path, min_frame_dest_path)
+# %%
